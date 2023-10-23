6b9e7fe8f6d60b816dd9176a33e9340156958b16
Cody Thompson <codylthomp@gmail.com>
2023-10-22 23:26:52 -0500

    more notes added, explaining my process

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require('inquirer'); //grabbing inquirer from our node files.
+const fs = require('fs'); //using our filesystem so we can read and write within our code.
 const icon = []; //array to store our icon photos, this is for the badge part of the assignment.
 
-// questions for our prompt
+// questions for our prompt, questions array 1 is for our normal questions
 const questions = ["What is your Project called?", "Can you Describe this project?", "Table of Contents", "Installation", "Usage", "Credits", "License" ,"Tests", "Questions"];
-const personalQuestions = ["Provide your Github username (no link)", "Provide your Email"];
+const personalQuestions = ["Provide your Github username (no link)", "Provide your Email"]; //personal questions comes at the end.
 
 
-function writeToFile(response) {
+function writeToFile(response) { //we start building our readme here!
 
-    response.license.forEach((license) => { //we handle license icons/badges here.
-        switch (license) {
+    response.license.forEach((license) => { //we handle license icons/badges here. this grabs the user input from our license question and adds the icons to an array if we have a match!
+        switch (license) { //chose to handle this here, as we would have to pass the selected Licenses back and forth with the way I am doing things. TODO, we could split our http link to include whatever we want on the badge instead of specifically typing every single one out. If we had more licenses to handle.
           case "Mit License":
             icon.push("![Static Badge](https://img.shields.io/badge/Mit%20License-blue)");
             console.log("Here I am!");
@@ -40,7 +40,7 @@ function writeToFile(response) {
       });
 
 console.log(icon); //testing icon array.
-//Start of writing our Readme out depending on user input.
+//Start of writing our Readme out depending on user input. from here we use some stylistic approaches to make our readme look like a readme! We are also exporting readmeTest as the name for sake of the project. we also push the icons to the right of the readme top to make it look less cluttered.
 fs.appendFile('READMETest.md', `# ${response.project}  <span style="float: right;">${icon.join('')}</span>
     
 ## Description:
@@ -90,12 +90,11 @@ Email: [${response.questions1}](${response.questions1})
  \n`, note =>  note ? console.error(note) : console.log('Professional ReadMe Created!'))   
 }
 
-// TODO: Create a function to initialize app
 
 
-function init() {
-    inquirer
-    .prompt([
+function init() { //on running our script init is invoked on line 152
+    inquirer //using inquirier
+    .prompt([ //using inquirer.prompt
         {
             type: 'input',
             message: questions[0],
@@ -123,7 +122,7 @@ function init() {
             name: 'credits',
           },
           {
-            type: 'checkbox',
+            type: 'checkbox', //here we use a checkbox as we want to select specific licenses without the user having to recall them.
             message: questions[6],
             name: 'license',
             choices: ['Mit License', 'Apache License 2.0', 'Eclipse Public License 2.0', 'Boost Software License 1.0', 'GNU General Public License v3.0']
@@ -144,21 +143,14 @@ function init() {
             name: 'questions1',
           },
     ])
-    .then((response) => writeToFile(response)
+    .then((response) => writeToFile(response) //once our .prompt is ran through, and data is collected we pass the data (response) to our writetofile function.
   );
 
 
 
 }
-init();
+init(); //on running node/our script file we go to our prompt.
 
 
 
 
-/*GIVEN a command-line application that accepts user input
-WHEN I enter my GitHub username
-THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-WHEN I enter my email address
-THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-WHEN I click on the links in the Table of Contents
-THEN I am taken to the corresponding section of the README  */
